8b85d88a01d87df905bcc12aea105cb47a9d1b73
luochujian <350481693@qq.com>
2017-03-27 12:09:28 +0800

    能爬取每个 keyword 第一页内容，容易被 ban

--- job_spider/spiders/job.py
+++ job_spider/spiders/job.py
@@ -5,6 +5,9 @@ from scrapy.http import HtmlResponse,Request
 from job_spider.settings import *
 from job_spider.items import Job
 from scrapy.http import FormRequest
+from cookielib import CookieJar
+import json
+import random
 
 
 class JobSpider(Spider):
@@ -13,6 +16,48 @@ class JobSpider(Spider):
     start_urls = [
         'https://www.lagou.com/'
     ]
+
+    headers = {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip,deflate,sdch,br',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.8,zh-TW;q=0.6,zh;q=0.4',
+        'Connection': 'keep-alive',
+    }
+
+    cookies = {
+        'user_trace_token': '20170326232733-7b144a277697485ba8693c95d7097ea5',
+        'LGUID': '20170326232734-bc595a50-1238-11e7-a251-525400f775ce',
+        'index_location_city': '%E6%B7%B1%E5%9C%B3',
+        'TG-TRACK-CODE': 'index_navigation',
+        'SEARCH_ID': '5951c5f55df64bbda9043fcd9ccc9a7b',
+        'Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6': '1490542064',
+        'Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6': '1490542122',
+        '_ga': 'GA1.2.1638570626.1490542064',
+        'LGRID': '20170326232832-dee75d3d-1238-11e7-956f-5254005c3644',
+        'JSESSIONID': '1CA02935D797115D2CF33B65E763316D',
+    }
+
+    user_agent_list = [\
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"\
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",\
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",\
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",\
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",\
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",\
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",\
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",\
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",\
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",\
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",\
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",\
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",\
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",\
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",\
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",\
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",\
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+       ]
 
     def parse(self, response):
         keywords = []
@@ -24,14 +69,22 @@ class JobSpider(Spider):
                 keyword = sel2.xpath('text()').extract()[0]
                 keywords.append(keyword)
 
-        # job_list_url = 'https://www.lagou.com/jobs/positionAjax.json?city=%E6%B7%B1%E5%9C%B3&needAddtionalResult=false'
-        job_list_url = 'https://www.lagou.com/jobs/positionAjax.json?city=深圳&needAddtionalResult=false'
+        job_list_url = 'https://www.lagou.com/jobs/positionAjax.json?city=%s&needAddtionalResult=false' % DEFAULT_CITY
         for keyword in keywords:
-            formdata = {'first': False, 'pn': 1, 'kd': keyword}
-            yield FormRequest(job_list_url, formdata=formdata, callback=self.parse_job)
+            ua = random.choice(self.user_agent_list)
+            self.headers['User-Agent'] = ua
+            formdata = {'first': 'false', 'pn': '1', 'kd': keyword}
+            yield FormRequest(job_list_url,
+                              formdata=formdata,
+                              cookies=self.cookies,
+                              headers=self.headers,
+                              callback=self.parse_job)
 
     def parse_job(self, response):
-        yield response
+        jsonresponse = json.loads(response.body_as_unicode())
+        job = Job()
+        job['name'] =jsonresponse['content']
+        yield job
 
     def parse_job_detail(self, response):
         job = Job()
@@ -53,3 +106,4 @@ class JobSpider(Spider):
 #
 # In [4]: r = FormRequest(url, formdata=frmdata)
 
+# job_list_url = 'https://www.lagou.com/jobs/positionAjax.json?city=%E6%B7%B1%E5%9C%B3&needAddtionalResult=false'
